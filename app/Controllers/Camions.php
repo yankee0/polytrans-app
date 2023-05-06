9c9fdced8aa3571654df3f31d81cadc3ef2aa276
yankee <yankeesuprem@gmail.com>
2023-05-06 18:29:56 +0000

    BUG sur modification immatriculation du camion

--- app/Controllers/Camions.php
+++ app/Controllers/Camions.php
@@ -12,12 +12,13 @@ class Camions extends BaseController
         session()->position = 'camions';
         $modele = new ModelsCamions();
         $donnee = [
-            'liste' => $modele->orderBy('immatriculation','ASC')->findAll()
+            'liste' => $modele->orderBy('immatriculation', 'ASC')->findAll()
         ];
-        return view('utils/camions/liste',$donnee);
+        return view('utils/camions/liste', $donnee);
     }
 
-    public function ajout(){
+    public function ajout()
+    {
         $donnee = $this->request->getPost();
         $rules = [
             'immatriculation' => [
@@ -25,24 +26,54 @@ class Camions extends BaseController
             ],
         ];
         if (!$this->validate($rules)) {
-            return redirect()->back()->with('notif',false)->with('message','Identifiants en doublons');
+            return redirect()->back()->with('notif', false)->with('message', 'Identifiants en doublons.');
         } else {
             $donnee['immatriculation'] = strtoupper($donnee['immatriculation']);
             if ((new ModelsCamions())->insert($donnee) == 0) {
-                return redirect()->back()->with('notif',true)->with('message','Ajout réussi.');
-            }else{
-                return redirect()->back()->with('notif',false)->with('message','Echec de l\'ajout.');
+                return redirect()->back()->with('notif', true)->with('message', 'Ajout réussi.');
+            } else {
+                return redirect()->back()->with('notif', false)->with('message', 'Echec de l\'ajout.');
             }
         }
     }
 
-    public function supprimer(){
+    public function supprimer()
+    {
         $id = $this->request->getVar('id');
         $modele = new ModelsCamions();
         if ($modele->delete($id)) {
-            return redirect()->back()->with('notif',true)->with('message','Suppression réussie');
+            return redirect()->back()->with('notif', true)->with('message', 'Suppression réussie.');
         } else {
-            return redirect()->back()->with('notif',false)->with('message','Echec de la suppression');
+            return redirect()->back()->with('notif', false)->with('message', 'Echec de la suppression.');
+        }
+    }
+
+    public function modifier(string $id)
+    {
+        $donnee = [
+            'camion' => (new ModelsCamions())->find($id),
+        ];
+        return view('utils/camions/modifier', $donnee);
+    }
+
+    public function enregistrer()
+    {
+        $donnee = $this->request->getPost();
+        $rules = [
+            'immatriculation' => [
+                'rules' => 'is_unique[camions.immatriculation,immatriculation,' . $donnee['immatriculation'] . ']|min_length[3]',
+            ],
+        ];
+        if (!$this->validate($rules)) {
+            return redirect()->back()->with('notif', false)->with('message', 'Identifiants en doublons.');
+        } else {
+            $modele = new ModelsCamions();
+            // dd($donnee);
+            if ($modele->where('immatriculation',$donnee['immatriculation'])->set($donnee)->update()) {
+                return redirect()->to(session()->root . '/camions')->with('notif', true)->with('message', 'Modification réussie.');
+            } else {
+                return redirect()->back()->with('notif', false)->with('message', 'Echec de la modification.');
+            }
         }
     }
 }
---
+++ app/Views/utils/camions/modifier.php
@@ -0,0 +1,43 @@
+<?= $this->extend('layouts/ui'); ?>
+<?= $this->section('titre'); ?>
+Liste des camions
+<?= $this->endSection(); ?>
+<?= $this->section('contenu'); ?>
+
+<script>
+  $(document).ready(function() {
+
+    $('#recherche').keyup(function() {
+      searchTable('tableau', 'recherche');
+    });
+    paginateTable('tableau', 15);
+  });
+</script>
+
+<div class="container-fluid p-0">
+
+  <h1 class="h3 mb-3"><strong>Gestion</strong> camions</h1>
+
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Modifier le immatriculation du camion</h5>
+
+      <form class="row" action="<?= base_url(session()->root . '/camions/modifier') ?>" method="post">
+        <div class="mb-3 col-sm-6 col-lg-3">
+          <input type="text" class="form-control" value="<?=set_value('immatriculation',$camion['immatriculation'])?>" required name="immatriculation" id="immatriculation" aria-describedby="helpId" placeholder="Immatriculation du camion">
+        </div>
+        <div class="mb-3 col-sm-6 col-lg-3">
+          <button type="submit" class="btn btn-primary d-flex justify-content-center align-items-center gap-2">
+            <i class="align-middle" data-feather="check"></i>
+            Enregistrer les modifications
+          </button>
+        </div>
+        <?= csrf_field() ?>
+      </form>
+    </div>
+  </div>
+
+</div>
+
+
+<?= $this->endSection(); ?>
