8c8697685d467ff90efc421f4d4d614cf1689114
yankee <yankeesuprem@gmail.com>
2023-05-23 12:03:15 +0000

    stylisation mail activation

--- app/Views/utils/mail/activation.php
+++ app/Views/utils/mail/activation.php
@@ -64,7 +64,9 @@
 <body>
   <div class="container">
     <div class="logo">
-      <img src="<?= base_url('img/logo.png') ?>" alt="Logo de POLY-TRANS SUARL">
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M11.5 8h-7a1 1 0 00-1 1v5a1 1 0 001 1h7a1 1 0 001-1V9a1 1 0 00-1-1zm-7-1a2 2 0 00-2 2v5a2 2 0 002 2h7a2 2 0 002-2V9a2 2 0 00-2-2h-7zm0-3a3.5 3.5 0 117 0v3h-1V4a2.5 2.5 0 00-5 0v3h-1V4z" clip-rule="evenodd"></path>
+      </svg>
     </div>
     <div class="content">
       <h2>Activation de compte</h2>
